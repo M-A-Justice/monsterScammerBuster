26c89241585af66e8d43b246c9600429ccdcf4a3
M-A-Justice <michae1jus7ice@gmail.com>
2021-09-22 15:02:43 -0400

    updated setInterval on worker processes in an attempt to fix data generation with faker creating duplicate entries

--- index.js
+++ index.js
@@ -11,33 +11,28 @@ try {
   console.error(err);
 }
 
-let interval = () => {
-  const threadCount = 1;
-  const threads = new Set();
-  console.log(`Running with ${threadCount} threads...`);
+const threadCount = 1;
+const threads = new Set();
+console.log(`Running with ${threadCount} threads...`);
 
-  for (let i = 0; i < threadCount; i += 1) {
-    threads.add(new Worker('./script.js'));
-  }
+for (let i = 0; i < threadCount; i += 1) {
+  threads.add(new Worker('./script.js'));
+}
 
-  for (let worker of threads) {
-    worker.on('error', (err) => {
-      throw err;
-    });
+for (let worker of threads) {
+  worker.on('error', (err) => {
+    throw err;
+  });
 
-    worker.on('exit', () => {
-      threads.delete(worker);
-      console.log(`Thread exiting, ${threads.size} running...`);
-      if (threads.size === 0) {
-        console.log('Process finished');
-        clearInterval(this);
-      }
-    });
+  worker.on('exit', () => {
+    threads.delete(worker);
+    console.log(`Thread exiting, ${threads.size} running...`);
+    if (threads.size === 0) {
+      console.log('Process finished');
+    }
+  });
 
-    worker.on('message', (msg) => {
-      console.log(msg);
-    });
-  }  
-};
-
-setInterval(interval, 3000);
+  worker.on('message', (msg) => {
+    console.log(msg);
+  });
+}  
